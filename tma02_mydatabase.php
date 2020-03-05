6c28c8ccddab679ed38ff1ae5c452dbe768d77a4
danger_2997 <alex_Zelea@yahoo.com>
2020-03-05 17:10:45 +0000

    Guest House Booking System
    Guest House Booking System

---
+++ tma02_mydatabase.php
@@ -0,0 +1,13 @@
+<?php
+// This file holds the credentials to connect to the database.
+if(!defined('ISITSAFETORUN')) {
+// This provides protection against file being called directly - if it is, ISITSAFETORUN will not be defined
+   die('This file does no useful work alone'); // and so this warning message will be issued
+}
+// If this point is reached, ISITSAFETORUN was defined and this file has been called from an appropriate place
+// Enter your database information 
+$mydatabase='';
+$username='';
+$password=''; // Note that is especially important for the password to be in single quotes
+$hostname = 'localhost';  
+?>
